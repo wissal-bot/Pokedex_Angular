178b224815f1c32abd289f1cecb65a5dc6c5f466
Mydmoov <denizot.myriam@gmail.com>
2024-01-20 13:31:32 +0100

    generate component

---
+++ mon-pokedex/src/app/pokemon-details/pokemon-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './pokemon-details.component.html',
+  styleUrl: './pokemon-details.component.css'
+})
+export class PokemonDetailsComponent {
+
+}
---
+++ mon-pokedex/src/app/pokemon-tab/pokemon-tab.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-tab',
+  standalone: true,
+  imports: [],
+  templateUrl: './pokemon-tab.component.html',
+  styleUrl: './pokemon-tab.component.css'
+})
+export class PokemonTabComponent {
+
+}
