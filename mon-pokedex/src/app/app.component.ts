f9e4c8c0ec33bd6664e90e7bda70c3f29bfe0287
Mydmoov <denizot.myriam@gmail.com>
2024-01-20 16:06:38 +0100

    Connexion avec lAPI opensource

--- mon-pokedex/src/app/app.component.ts
+++ mon-pokedex/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {PokemonTabComponent} from "./pokemon-tab/pokemon-tab.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, PokemonTabComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- mon-pokedex/src/app/app.routes.ts
+++ mon-pokedex/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PokemonTabComponent } from './pokemon-tab/pokemon-tab.component';
+import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PokemonTabComponent },
+  { path: 'pokemon/:id', component: PokemonDetailsComponent },
+];
